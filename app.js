ed8fc40e10e347811a97ae62f4e051d6b152ec4c
Alibaba-sell <dasdamirovaslan99@gmail.com>
2024-10-05 11:51:00 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,15 @@
+let home = true
+function change_status(home){
+    if(home == true){
+        home = false 
+    }
+    else{
+        
+    }
+}
+if (home ==  true){
+    console.log("You're home")
+}
+else{
+    console.log("You're not at home")
+}
